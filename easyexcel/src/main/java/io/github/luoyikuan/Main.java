969ca3802f888bb838f09c0db9908c4727bb1ecc
lyk <lyk98@qq.com>
2024-04-09 16:41:11 +0800

    easyexel

---
+++ easyexcel/src/main/java/io/github/luoyikuan/Main.java
@@ -0,0 +1,11 @@
+package io.github.luoyikuan;
+
+/**
+ * @author Y.K. LUO
+ * @since 2024/4/9
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
